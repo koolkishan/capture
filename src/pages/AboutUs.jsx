083dbc5877baabb532275ae7a8b8f084f4e6e259
koolkishan <kishansheth21@gamil.com>
2020-11-22 23:15:14 +0530

    Added Services Section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/Services";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
