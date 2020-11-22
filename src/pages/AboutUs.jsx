02df78ccc61e2672d67f12b59d2970b54563579d
koolkishan <kishansheth21@gamil.com>
2020-11-22 23:37:14 +0530

    Added FAQ SectionComponent

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
-import ServicesSection from "../components/Services";
+import FaqSection from "../components/FaqSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
