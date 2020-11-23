369b4828c70b1ace19086df4311fadc6ddd89d73
koolkishan <kishansheth21@gamil.com>
2020-11-23 15:15:38 +0530

    Added Animations

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,32 +4,32 @@ import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "./../styles";
 //Framer Motion
 import { motion } from "framer-motion";
-
+import { titleAnim, fade, photoAnim } from "../animation";
 const AboutSection = () => {
   return (
     <About>
       <Description>
         <motion.div className="title">
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img src={home1} alt="guy with a camera" variants={photoAnim} />
       </Image>
     </About>
   );
