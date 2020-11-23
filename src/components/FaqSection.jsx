a7e73f739f0d503925db4830482ac9d00948de6e
koolkishan <kishansheth21@gamil.com>
2020-11-23 16:37:31 +0530

    Implemented FAQ Toggle and added Animations

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Toggle from "./Toggle";
 import styled from "styled-components";
 import { About } from "./../styles";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
   return (
@@ -8,50 +10,50 @@ const FaqSection = () => {
       <h2>
         Any Questions? <span>FAQ</span>{" "}
       </h2>
-      <div className="question">
-        <h4>How Do i Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
-            consequatur!
-          </p>
-        </div>
-        <div className="fag-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
-            consequatur!
-          </p>
-        </div>
-        <div className="fag-line"></div>
-      </div>
-      <div className="question">
-        <h4>Deifferent Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
-            consequatur!
-          </p>
-        </div>
-        <div className="fag-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products do you offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
-            consequatur!
-          </p>
-        </div>
-        <div className="fag-line"></div>
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How Do i Start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
+              consequatur!
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+      <AnimateSharedLayout>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
+              consequatur!
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+      <AnimateSharedLayout>
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
+              consequatur!
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+      <AnimateSharedLayout>
+        <Toggle title="What Products do you offer?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae,
+              consequatur!
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
