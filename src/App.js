aab5e4db17b634c8a8cece1c0269ae1806e0be93
koolkishan <kishansheth21@gamil.com>
2020-11-23 09:48:48 +0530

    Added Navbar Component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import React from "react";
 
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyles";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
