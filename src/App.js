f6d951d50e4e2959baea34597b57c72105f31049
koolkishan <kishansheth21@gamil.com>
2020-11-23 10:06:52 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
-
 //Global Styles
-
-import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyles";
+//Pages
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Components
 import Nav from "./components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
